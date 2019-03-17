03e04c9acbce51192884de76ab615b0270304603
ramalla <ramesh.malla@zalando.de>
2019-03-17 13:18:06 +0100

    GH-2 Fixing Shit storm

---
+++ src/main/kotlin/com/tafel/missions/tafelmissions/TafelMissionsApplication.kt
@@ -0,0 +1,11 @@
+package com.tafel.missions.tafelmissions
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class TafelMissionsApplication
+
+fun main(args: Array<String>) {
+	runApplication<TafelMissionsApplication>(*args)
+}
